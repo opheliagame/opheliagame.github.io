cf939b21457405a273c7d29f9eb0c3e0f6e06d69
opheliagame <opheliagame@users.noreply.github.com>
2023-01-29 17:48:24 +0000

    deploy: d5e08aaca5a11b99074ff89b1cd3d9ad7e1d78d6

--- assets/js/misc.js
+++
@@ -1,6 +0,0 @@
-// animate river in whitespace.njk
-function animateRiver(river) {
-  const first = river.children[0]
-  river.removeChild(first)
-  river.appendChild(first)
-}
--- assets/js/sign.js
+++
@@ -1,6 +0,0 @@
-fetch('https://gist.githubusercontent.com/opheliagame/0e5a61bf8a6654f90f3032d2b8d22f3b/raw/826d9566805b1d9fb3fcc5ba4e7038648f45fc1d/sign.json')
-.then(response => response.json())
-.then(data => {
-  const sign = data.sign
-  console.log(...sign)
-})
