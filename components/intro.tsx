b2a7b5b9d666641eb134239ea1bc2a5e1cd334d3
opheliagame <21999998+opheliagame@users.noreply.github.com>
2023-10-13 00:04:52 +0900

    initial commit nextjs blog starter

---
+++ components/intro.tsx
@@ -0,0 +1,12 @@
+import { CMS_NAME } from '../lib/constants'
+
+const Intro = () => {
+  return (
+    <section className="flex-col mt-16 mb-16 md:mb-12">
+      <h2>this site is under construction 🛠️🪛</h2>
+      <h2>thoughts and notes can be found <a className='underline' target='_blank' href="https://opheliagame.github.io/notes/">here</a></h2>
+    </section>
+  )
+}
+
+export default Intro
