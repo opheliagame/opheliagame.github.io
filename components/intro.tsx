3a17e027866112a844c3acd47eb772853306ab81
Anushka Trivedi <21999998+opheliagame@users.noreply.github.com>
2023-10-15 22:55:06 +0900

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,7 +4,7 @@ const Intro = () => {
   return (
     <section className="flex-col mt-16 mb-16 md:mb-12">
       <h2>this site is under construction 🛠️🪛</h2>
-      <h2>thoughts and notes can be found <a className='underline' target='_blank' href="https://opheliagame.github.io/notes/">here</a></h2>
+      <h2>thoughts and notes can be found <a className='underline' target='_blank' href="https://opheliagame-notes.netlify.app/">here</a></h2>
     </section>
   )
 }
