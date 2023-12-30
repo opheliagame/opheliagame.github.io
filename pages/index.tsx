7f0d65acdb492a191e31892d6bcd949aa8ccbf3b
opheliagame <21999998+opheliagame@users.noreply.github.com>
2023-12-30 12:43:48 +0530

    SSR to CSR cos SSR doesn't work on github pages!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,53 @@
 import Layout from '../components/layout'
 import GenerativeGrid, { CSSGrid, Point, QuadTree, Rectangle } from '../components/generative-grid'
+import { useEffect, useState } from 'react'
 
 
 
-export default function Index(props) {
+export default function Index() {
+
+  const [data, setData] = useState(null)
+
+  useEffect(()  => {
+    let quadTree = new QuadTree(new Rectangle(200, 200, 200, 200), 2)  
+    for (let i = 0; i < 10; i++) {
+      let p = new Point(Math.random() * 200 * 2, Math.random() * 200 * 2)
+      quadTree.insert(p)
+    }
+    let cssgrid = new CSSGrid(quadTree, 200*2, 200*2)
+    let cssgridAreaString = cssgrid.getGridAreaString()
+  
+    let gridContent = [
+      {title: 'opheliagame', url: '/'},
+      {title: 'about', url: '/about'},
+      {title: 'thoughts', url: 'https://opheliagame-notes.netlify.app/'},
+      {title: 'concrete poetry', url: '/concrete-poetry'},
+      {title: 'instagram', url: 'https://www.instagram.com/ophelia.game/'},
+      {title: 'twitter', url: 'https://twitter.com/oopheliagame'},
+      {title: 'youtube', url: 'https://www.youtube.com/channel/UCPGhpghIHB7fX3xLtlNaRFg'},
+      {title: 'twitch', url: 'https://www.twitch.tv/opheliagame'}
+    ]
+  
+    let colors =  ['#DB2B39', '#00A878', '#F3A712', '#3066BE', '#79ADDC']
+  
+    
+    setData({
+      'cssgridAreaString': cssgridAreaString, 
+      'gridContent': gridContent, 
+      'colors': colors
+    })
+
+  }, [])
+
   return (
+    data ?
     <>
       <Layout>
-        <GenerativeGrid cssgridAreaString={props.cssgridAreaString} gridContent={props.gridContent} colors={props.colors} />
+        <GenerativeGrid cssgridAreaString={data.cssgridAreaString} gridContent={data.gridContent} colors={data.colors} />
       </Layout>
+    </> : 
+    <>
     </>
   )
 }
 
-export async function getServerSideProps() {
-  let quadTree = new QuadTree(new Rectangle(200, 200, 200, 200), 2)  
-  for (let i = 0; i < 10; i++) {
-    let p = new Point(Math.random() * 200 * 2, Math.random() * 200 * 2)
-    quadTree.insert(p)
-  }
-  let cssgrid = new CSSGrid(quadTree, 200*2, 200*2)
-  let cssgridAreaString = cssgrid.getGridAreaString()
-
-  let gridContent = [
-    {title: 'opheliagame', url: '/'},
-    {title: 'about', url: '/about'},
-    {title: 'thoughts', url: 'https://opheliagame-notes.netlify.app/'},
-    {title: 'concrete poetry', url: '/concrete-poetry'},
-    {title: 'instagram', url: 'https://www.instagram.com/ophelia.game/'},
-    {title: 'twitter', url: 'https://twitter.com/oopheliagame'},
-    {title: 'youtube', url: 'https://www.youtube.com/channel/UCPGhpghIHB7fX3xLtlNaRFg'},
-    {title: 'twitch', url: 'https://www.twitch.tv/opheliagame'}
-  ]
-
-  let colors =  ['#DB2B39', '#00A878', '#F3A712', '#3066BE', '#79ADDC']
-
-  return {
-    props: {
-      'cssgridAreaString': cssgridAreaString, 
-      'gridContent': gridContent, 
-      'colors': colors
-    }
-  }
-}
